7f358180906708771e134c7e971d8cb6285d69b4
letsgo247 <letsgo247@hanmail.net>
2021-05-21 23:16:47 +0900

    Initial commit

---
+++ main/urls.py
@@ -0,0 +1,19 @@
+from django.conf.urls import url
+# from django.contrib import admin
+from . import views
+
+# from django.contrib.sitemaps.views import sitemap
+# from .sitemaps import StaticViewSitemap
+
+
+app_name = 'main'
+
+# sitemaps = {
+#     'static': StaticViewSitemap,
+# }
+
+urlpatterns = [
+    url(r'^$', views.index, name = 'index'),
+    # url(r'^generate.+$', views.generate, name = 'generate'),
+    # url(r'^sitemap\.xml$', sitemap, {'sitemaps': sitemaps}, name='django.contrib.sitemaps.views.sitemap')
+]
