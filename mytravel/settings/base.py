dabe66412d1e8d5d3ce61de22adcad1bf1a97b20
letsgo247 <letsgo247@hanmail.net>
2021-06-27 01:00:12 +0900

    local/deploy 분리

--- mytravel/settings/base.py
+++ mytravel/settings/base.py
@@ -13,32 +13,8 @@ import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
-BASE_DIR = Path(__file__).resolve().parent.parent
+BASE_DIR = Path(__file__).resolve().parent.parent.parent
 
-
-import environ
-env = environ.Env(
-    # set casting, default value
-    DEBUG=(bool, False)
-)
-# reading .env file
-environ.Env.read_env(
-    env_file= os.path.join(BASE_DIR, '.env')
-)
-
-
-
-# Quick-start development settings - unsuitable for production
-# See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
-
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = env('SECRET_KEY')
-
-# SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = env('DEBUG')
-# DEBUG = True
-
-ALLOWED_HOSTS = ['127.0.0.1', '141.164.37.184']
 
 
 # Application definition
@@ -83,17 +59,6 @@ TEMPLATES = [
 ]
 
 WSGI_APPLICATION = 'mytravel.wsgi.application'
-
-
-# Database
-# https://docs.djangoproject.com/en/3.2/ref/settings/#databases
-
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
 
 
 # Password validation
---
+++ mytravel/settings/deploy.py
@@ -0,0 +1,42 @@
+from .base import *
+
+
+import environ
+env = environ.Env(
+    # set casting, default value
+    DEBUG=(bool, False)
+)
+# reading .env file
+environ.Env.read_env(
+    env_file= os.path.join(BASE_DIR, '.env')
+)
+
+
+
+# Quick-start development settings - unsuitable for production
+# See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
+
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = env('SECRET_KEY')
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = False
+
+ALLOWED_HOSTS = ['141.164.37.184']
+
+
+
+
+# Database
+# https://docs.djangoproject.com/en/3.2/ref/settings/#databases
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'mariadb',
+        'USER': 'root',
+        'PASSWORD': 'password1234',
+        'HOST': 'mariadb_container',
+        'PORT': '3306',
+    }
+}
