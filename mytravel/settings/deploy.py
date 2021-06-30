5021d6a8c28ee9a247aabe8ca80b4ca0017ca895
letsgo247 <letsgo247@hanmail.net>
2021-06-30 12:02:46 +0900

    docker secret

--- mytravel/settings/deploy.py
+++ mytravel/settings/deploy.py
@@ -12,12 +12,19 @@ environ.Env.read_env(
 )
 
 
+def read_secret(secret_file):
+    file = open('/run/secrets/' + secret_file)
+    secret = file.read()
+    secret = secret.rstrip().lstrip()
+    file.close()
+    return secret
+
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = env('SECRET_KEY')
+SECRET_KEY = read_secret('DJANGO_SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
