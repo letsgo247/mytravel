2f5e36d1e700a83406d5b9d150221149628aca8d
letsgo247 <letsgo247@hanmail.net>
2021-07-24 21:39:23 +0900

    re-set

--- mytravel/settings/deploy.py
+++ mytravel/settings/deploy.py
@@ -51,6 +51,6 @@ ALLOWED_HOSTS = ['*']
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'NAME': BASE_DIR / 'DB/db.sqlite3',
     }
 }
